a6e30c17de4297c709061dbe5a520ae5db3c9fc6
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2025-04-21 01:01:09 -0300

    Valores Numéricos Aleatorios(modificado) y Tipos De Procesador

--- tp2_4.c
+++ tp2_4.c
@@ -7,13 +7,30 @@ typedef struct{
     int anio;           // Año de fabricación (valor entre 2015 y 2024) 
     int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8) 
     char *tipo_cpu;     // Tipo de procesador (apuntador a cadena de caracteres) 
-} compu; 
+}compu; 
+
+char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+int generarVelocidad (){
+    return 1 + rand() % 3;
+}
+
+int generarYear (){
+    return 2015 + rand() % 2024;
+}
+
+int generarCantidadNucleos (){
+    return 1 + rand() % 8;
+}
 
 int main (){
     srand(time(NULL));
-    compu Compu;
-    Compu.velocidad = 1 + rand() % 3;
-    printf("%d ", Compu.velocidad);
+    compu PC = {
+        generarVelocidad(),
+        generarYear(),
+        generarCantidadNucleos(),
+        tipos[rand()%6]
+    }; 
     
     return 0;
 }
